64ee85691fa980ebeae203b66c72953f19bb186c
민규 <xiahtic73@gmail.com>
2023-08-25 00:57:07 +0900

    Tailwind Setting framer-motion, swiper 패키지 설치

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { env } from "node:process";
+
+const idDev = env.NODE_ENV === "development";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    devSourcemap: true,
+    modules: {
+      generateScopedName: idDev ? "[name]_[local]__[hash:base64:5]" : "[hash:base64:4]",
+    },
+  },
   resolve: {
     alias: [
       { find: "@", replacement: "/src" },
@@ -11,9 +20,9 @@ export default defineConfig({
       { find: "@c", replacement: "/src/components" },
       { find: "@l", replacement: "/src/layout" },
       { find: "@p", replacement: "/src/pages" },
-      { find: "@s", replacement: "/src/style" },
+      { find: "@s", replacement: "/src/styles" },
       { find: "@u", replacement: "/src/utils" },
       { find: "@h", replacement: "/src/hooks" },
     ],
   },
-})
+});
